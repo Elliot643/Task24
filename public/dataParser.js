dbcf1b5d4f78c6872268043c7fc41981884fb3fd
Elliot643 <elliot643@hotmail.com>
2019-10-11 10:17:41 +0200

    change to index

--- public/dataParser.js
+++ public/dataParser.js
@@ -1,6 +1,5 @@
 function placeInfo(){
     $.getJSON("data.json", function(jsonData){
-        console.log("in func");
         document.getElementById("titlePlacement").innerHTML=jsonData.name+" - CV";;
 
     });
